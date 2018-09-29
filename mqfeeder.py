49dd53dbfe955e57a58441078e3f2a02daec81bf
zya <1328410180@qq.com>
2018-09-29 17:23:58 +0800

    verify with baidu api

--- mqfeeder.py
+++ mqfeeder.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from qqbot.utf8logger import INFO, ERROR
-from urllib.request import urlopen
-from urllib.parse import urlencode
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode, quote
 from queue import Queue
 import re
 import threading as td
@@ -10,6 +10,7 @@ import time
 import socket
 import ssl
 import os
+import json
 TALK_FLAG = True
 MY_SOCKET = None
 MSG_Queue_Dict = {}
@@ -60,6 +61,34 @@ def tcplink(connStream, addr):
     connStream.close()
     INFO('Close connection with %s:%s...' % addr)
 
+def verify(word):
+    ret = ''
+    api_url = 'https://aip.baidubce.com/rest/2.0/antispam/v2/spam'
+    paramas = {
+        'access_token': 'YourAccessToken',
+        'content':  word
+    }
+    paramas = urlencode(paramas).encode('utf-8')
+    labels_type = {
+        1:'暴恐违禁',
+        2:'文本色情',
+        3:'政治敏感',
+        4:'恶意推广',
+        5:'低俗辱骂',
+        6:'低质灌水',
+    }
+    response = Request(api_url,data=paramas)
+    response = urlopen(response).read()
+    for error in json.loads(response)['result']['reject']:
+        for hit in error['hit']:
+            ret = ret + hit + ' '
+        ret = ret + ' 涉嫌' + labels_type[error['label']] + '\n'
+    for error in json.loads(response)['result']['review']:
+        for hit in error['hit']:
+            ret = ret + hit
+        ret = ret + ' 涉嫌' + labels_type[error['label']] + '\n'
+    return ret
+
 
 def onPlug(bot):
     global MY_SOCKET
@@ -72,16 +101,20 @@ def onPlug(bot):
 
 # qqbot onMessage event
 def onQQMessage(bot, contact, member, content):
-    global TALK_FLAG, MSG_Queue_Dict
-    if re.search(r"#system ", content, re.I):
-        if re.search(r"start", content):
-            TALK_FLAG = True
-        elif re.search(r"stop", content):
-            TALK_FLAG = False
-    # send message to queue
-    if TALK_FLAG and not bot.isMe(contact, member):
-        try:
-            for queue in MSG_Queue_Dict.values():
-                queue.put(content)
-        except:
-            ERROR("Unexpected error in onQQMessage:" + str(sys.exc_info()))
+    try:
+        global TALK_FLAG, MSG_Queue_Dict
+        if re.search(r"#system ", content, re.I):
+            if re.search(r"start", content):
+                TALK_FLAG = True
+            elif re.search(r"stop", content):
+                TALK_FLAG = False
+        # send message to queue
+        if TALK_FLAG and content and not bot.isMe(contact, member):
+            verify_result = verify(content)
+            if verify_result:
+                bot.SendTo(contact, verify_result[:-1])
+            else:
+                for queue in MSG_Queue_Dict.values():
+                    queue.put(content)
+    except:
+        ERROR("Unexpected error in onQQMessage:" + str(sys.exc_info()))
